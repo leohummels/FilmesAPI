d8ea3a2f005b10d599b5063f534f1020db339e05
leohummels <lsousa767@gmail.com>
2022-07-07 13:40:52 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage ="Campo diretor é obrigatório")]
+        public string Diretor { get; set; }
+        [StringLength(35, ErrorMessage = "Campo gênero deve conter no máximo 35 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "Duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
